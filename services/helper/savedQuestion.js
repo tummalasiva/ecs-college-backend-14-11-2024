dea2ac6f44e8163d6795d61e7089c822d2d8efbc
Leelakshig <leelakshi.g@webspruce.com>
2024-09-30 16:10:21 +0530

    const variabe

--- services/helper/savedQuestion.js
+++ services/helper/savedQuestion.js
@@ -6,11 +6,11 @@ const { uploadFileToS3, deleteFile } = require("../../helper/helpers");
 module.exports = class SavedQuestionHelper {
   static async create(req) {
     try {
-      const file = "";
+      let file = "";
       if (req.files && req.files.image) {
         file = await uploadFileToS3(req.files.image);
       }
-      const savedQuestion = await savedQuestionQuery.create({
+      let savedQuestion = await savedQuestionQuery.create({
         ...req.body,
         createdBy: req.employee,
         image: file,
