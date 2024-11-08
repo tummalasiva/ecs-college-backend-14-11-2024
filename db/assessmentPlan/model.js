03a1a9a620654b44812ba7cb707286c5768e6007
gnavneet <gauravkumar.navneet@webspruce.com>
2024-11-08 18:12:07 +0530

    updated assessment plan schema and api

--- db/assessmentPlan/model.js
+++ db/assessmentPlan/model.js
@@ -35,13 +35,13 @@ const assessmentExamSchema = new mongoose.Schema({
     type: Boolean,
     required: true,
   },
-  questionDesribution: {
-    type: String,
-    enum: ["per_student", "batch"],
-    required: function () {
-      this.multipleQuestionsCanBeSet;
-    },
-  },
+  // questionDesribution: {
+  //   type: String,
+  //   enum: ["per_student", "batch"],
+  //   required: function () {
+  //     this.multipleQuestionsCanBeSet;
+  //   },
+  // },
 });
 
 const assessmentPlanSchema = new mongoose.Schema({
